81d7dacfe2e783c9d0b0f3e0fb32957f2f7073f5
Narendra Kumar <135869073+narendrakumar5264@users.noreply.github.com>
2025-01-28 23:03:52 +0530

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -55,7 +55,7 @@ export default function Header() {
     <header className="bg-white shadow-md fixed top-0 left-0 w-full z-50">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         {/* Logo */}
-        <Link to="/" className="text-2xl font-bold text-blue-600">
+        <Link to="/" className="text-2xl font-bold md:ml-10 text-blue-600">
           <span className='text-slate-800'>NK</span>Estate
         </Link>
 
@@ -73,7 +73,7 @@ export default function Header() {
         </nav>
 
         {/* Right Section */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center ml-20 md:mr-9 gap-9">
           <Link to="/create-listing">
             <button className="bg-blue-500 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-blue-600 transition">
               Sell/Rent
@@ -114,8 +114,8 @@ export default function Header() {
               )}
             </div>
           ) : (
-            <Link to="/sign-in" className="text-gray-700 hover:text-blue-500">
-              Sign In
+            <Link to="/sign-in" className="text-gray-700  hover:text-blue-500">
+              LogIn 
             </Link>
           )}
         </div>
